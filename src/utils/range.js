ac8a4f699b4e4b87146be766a46b1189117db8af
adefuyeabayomi <63407534+adefuyeabayomi@users.noreply.github.com>
2023-12-29 10:14:03 +0100

    Basic testing setup with vitest and @testing-library/vue

---
+++ src/utils/range.js
@@ -0,0 +1,9 @@
+
+function createRange(start,end){
+    let range = [];
+    for(let counter = start; counter <= end; counter++){
+        range.push(counter);
+    }
+    return range;
+}
+export {createRange}
